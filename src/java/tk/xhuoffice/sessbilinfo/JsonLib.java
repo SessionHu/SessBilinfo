b3831a4f1d3ca9c0ae123a84891d6a054370e706
Session小胡 <102411014+SessionHu@users.noreply.github.com>
2023-08-07 09:11:56 +0000

    fix(JsonLib): 常量的错误命名

--- src/java/tk/xhuoffice/sessbilinfo/JsonLib.java
+++ src/java/tk/xhuoffice/sessbilinfo/JsonLib.java
@@ -7,21 +7,21 @@ import com.google.gson.JsonObject;
 
 public class JsonLib {
     
-    public static final Gson gson = new Gson();
+    public static final Gson GSON = new Gson();
     
     // 格式化 JSON
     public static String formatJson(String inputJson) {
-        return new GsonBuilder().setPrettyPrinting().create().toJson(gson.fromJson(inputJson,JsonObject.class));
+        return new GsonBuilder().setPrettyPrinting().create().toJson(GSON.fromJson(inputJson,JsonObject.class));
     }
     
     // eg: 获取 {"code":6} 中 "code" 的数值
     public static int getRootObjectInt(String inputJson, String str) {
-        return gson.fromJson(inputJson,JsonObject.class).get(str).getAsInt();
+        return GSON.fromJson(inputJson,JsonObject.class).get(str).getAsInt();
     }
     
     // eg: 获取 {"message":"test"} 中 "message" 的内容
     public static String getRootObjectString(String inputJson, String str) {
-        return gson.fromJson(inputJson,JsonObject.class).get(str).getAsString();
+        return GSON.fromJson(inputJson,JsonObject.class).get(str).getAsString();
     }
     
     /* eg: 获取下面这段 JSON 中 "like_num" 的数值
@@ -32,7 +32,7 @@ public class JsonLib {
         }
     */
     public static int getSubObjectInt(String inputJson, String obj1, String str) {
-        return gson.fromJson(inputJson,JsonObject.class).getAsJsonObject(obj1).get(str).getAsInt();
+        return GSON.fromJson(inputJson,JsonObject.class).getAsJsonObject(obj1).get(str).getAsInt();
     }
     
     /* eg: 获取下面这段 JSON 中 "name" 的内容
@@ -45,7 +45,7 @@ public class JsonLib {
         }
     */
     public static String getSubSubObjectString(String inputJson, String obj1, String obj2, String str) {
-        return gson.fromJson(inputJson,JsonObject.class).getAsJsonObject(obj1).getAsJsonObject(obj2).get(str).getAsString();
+        return GSON.fromJson(inputJson,JsonObject.class).getAsJsonObject(obj1).getAsJsonObject(obj2).get(str).getAsString();
     }
     
     /* eg: 获取下面这段 JSON 中 "fans" 的数值
@@ -58,7 +58,7 @@ public class JsonLib {
         }
     */
     public static int getSubSubObjectInt(String inputJson, String obj1, String obj2, String str) {
-        return gson.fromJson(inputJson,JsonObject.class).getAsJsonObject(obj1).getAsJsonObject(obj2).get(str).getAsInt();
+        return GSON.fromJson(inputJson,JsonObject.class).getAsJsonObject(obj1).getAsJsonObject(obj2).get(str).getAsInt();
     }
     
     /* eg: 获取下面这段 JSON 中 "current_level" 的数值
@@ -73,7 +73,7 @@ public class JsonLib {
         }
     */
     public static int getSubSubSubObjectInt(String inputJson, String obj1, String obj2, String obj3, String str) {
-        return gson.fromJson(inputJson,JsonObject.class).getAsJsonObject(obj1).getAsJsonObject(obj2).getAsJsonObject(obj3).get(str).getAsInt();
+        return GSON.fromJson(inputJson,JsonObject.class).getAsJsonObject(obj1).getAsJsonObject(obj2).getAsJsonObject(obj3).get(str).getAsInt();
     }
     
 }
