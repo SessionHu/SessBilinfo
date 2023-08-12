fa93bffeb0b53e01418caf32c36ba0c7723a5828
Session小胡 <102411014+SessionHu@users.noreply.github.com>
2023-08-12 16:17:15 +0800

    feat(UserInfo): 增加用户置顶视频日期与简介显示

--- src/java/tk/xhuoffice/sessbilinfo/NumFormat.java
+++ src/java/tk/xhuoffice/sessbilinfo/NumFormat.java
@@ -2,9 +2,14 @@ package tk.xhuoffice.sessbilinfo;
 
 import java.text.NumberFormat;
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.Instant;
+import java.time.ZoneId;
 import java.util.Locale;
 
 public class NumFormat {
+    
+    public static final ZoneId ZONE_HK = ZoneId.of("Asia/Hong_Kong");
     
     public static String time(int seconds) {
         Duration duration = Duration.ofSeconds(seconds);
@@ -19,6 +24,23 @@ public class NumFormat {
     
     public static String num(int num) {
         return NumberFormat.getInstance(Locale.US).format(num);
+    }
+    
+    public static String date(long timestamp) {
+        // 读取时间戳
+        Instant instant = Instant.ofEpochSecond(timestamp);
+        // 转换为日期
+        LocalDateTime date = LocalDateTime.ofInstant(instant,ZONE_HK);
+        int y = date.getYear(); // 年
+        int m = date.getMonthValue(); // 月
+        int d = date.getDayOfMonth(); // 日
+        int cY = LocalDateTime.now(ZONE_HK).getYear(); // 当前年份
+        // 返回数据
+        if(y==cY) {
+            return m+"-"+d;
+        } else {
+            return y+"-"+m+"-"+d;
+        }
     }
     
 }
