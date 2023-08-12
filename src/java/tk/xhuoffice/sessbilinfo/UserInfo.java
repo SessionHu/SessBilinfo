30d6045c1f5605c175a50dc1228e32a4f9ce0e99
Session小胡 <102411014+SessionHu@users.noreply.github.com>
2023-08-12 16:22:46 +0800

    fix(UserInfo): 增加遗漏的分号

--- src/java/tk/xhuoffice/sessbilinfo/UserInfo.java
+++ src/java/tk/xhuoffice/sessbilinfo/UserInfo.java
@@ -146,7 +146,7 @@ public class UserInfo {
             String date = NumFormat.date(pubdate); // 发布时间
             // 输出处理结果
             String topinfo = "";
-            topinfo += "[INFO] 置顶\n"
+            topinfo += "[INFO] 置顶\n";
             topinfo += "[INFO] "+title+"\n";
             topinfo += "[INFO] AV号 "+avid+"   "+date+"   时长 "+playtime+"\n";
             topinfo += "[INFO] 播放 "+strView+"   弹幕 "+strDanmaku+"\n";
