fcf3cb53ec2b3a036dc2632f0fb5c6f758c0410d
Session小胡 <102411014+SessionHu@users.noreply.github.com>
2023-08-14 13:10:43 +0800

    fix(Http): 导入 PrintWriter 与 StringWriter 类

--- src/java/tk/xhuoffice/sessbilinfo/Http.java
+++ src/java/tk/xhuoffice/sessbilinfo/Http.java
@@ -2,6 +2,8 @@ package tk.xhuoffice.sessbilinfo;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import tk.xhuoffice.sessbilinfo.Logger;
