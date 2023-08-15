d87b0a4e15605d9004348bde27f5374a02637f05
Session小胡 <102411014+SessionHu@users.noreply.github.com>
2023-08-15 14:32:49 +0800

    feat(JsonLib): 提取 Json 中数组的对象作为单独 Json

--- src/java/tk/xhuoffice/sessbilinfo/JsonLib.java
+++ src/java/tk/xhuoffice/sessbilinfo/JsonLib.java
@@ -1,9 +1,12 @@
 package tk.xhuoffice.sessbilinfo;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.util.stream.StreamSupport;
 
 
 public class JsonLib {
@@ -18,7 +21,7 @@ public class JsonLib {
     // 获取 String
     public static String getString(String inputJson, String... path) {
         JsonElement element = GSON.fromJson(inputJson, JsonObject.class);
-        for (String key : path) {
+        for(String key : path) {
             element = element.getAsJsonObject().get(key);
         }
         return element.getAsString();
@@ -27,7 +30,7 @@ public class JsonLib {
     // 获取 int
     public static int getInt(String inputJson, String... path) {
         JsonElement element = GSON.fromJson(inputJson, JsonObject.class);
-        for (String key : path) {
+        for(String key : path) {
             element = element.getAsJsonObject().get(key);
         }
         return element.getAsInt();
@@ -36,9 +39,20 @@ public class JsonLib {
     // 获取 long
     public static long getLong(String inputJson, String... path) {
         JsonElement element = GSON.fromJson(inputJson, JsonObject.class);
-        for (String key : path) {
+        for(String key : path) {
             element = element.getAsJsonObject().get(key);
         }
         return element.getAsLong();
     }
+    
+    // 获取数组中的对象作为单独的 Json
+    public static String[] getArrayObject(String json, String arr) {
+        JsonParser parser = new JsonParser();
+        JsonObject jsonObject = parser.parse(json).getAsJsonObject();
+        JsonArray jsonArray = jsonObject.getAsJsonArray(arr);
+        return StreamSupport.stream(jsonArray.spliterator(), false)
+                .map(GSON::toJson)
+                .toArray(String[]::new);
+    }
+    
 }
