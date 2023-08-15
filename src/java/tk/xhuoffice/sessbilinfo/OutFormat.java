d90a466d6dc053533e1b52d15d8658a8761aa9ff
Session小胡 <102411014+SessionHu@users.noreply.github.com>
2023-08-15 14:46:04 +0800

    feat(UserInfo): 增加用户代表作显示

--- src/java/tk/xhuoffice/sessbilinfo/OutFormat.java
+++ src/java/tk/xhuoffice/sessbilinfo/OutFormat.java
@@ -17,7 +17,7 @@ public class OutFormat {
         if (duration.toMinutes() >= 60) {
             formattedTime = String.format("%02d:%02d:%02d", duration.toHours(), duration.toMinutes() % 60, duration.getSeconds() % 60);
         } else {
-            formattedTime = String.format("%d:%02d", duration.toMinutes(), duration.getSeconds() % 60);
+            formattedTime = String.format("%02d:%02d", duration.toMinutes(), duration.getSeconds() % 60);
         }
         return formattedTime;
     }
