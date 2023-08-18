69ea08ac950841bb0c8186c97c7a8f4d487c9f7b
Session小胡 <102411014+SessionHu@users.noreply.github.com>
2023-08-18 17:41:28 +0800

    fix(CookieFile): 写入文件时抛出 FileNotFoundException

--- src/java/tk/xhuoffice/sessbilinfo/CookieFile.java
+++ src/java/tk/xhuoffice/sessbilinfo/CookieFile.java
@@ -5,32 +5,52 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import tk.xhuoffice.sessbilinfo.Logger;
+import tk.xhuoffice.sessbilinfo.OutFormat;
 
 public class CookieFile {
 
     public static final long COOKIE_EXPIRE_TIME = 30 * 60 * 1000; // 30 min
     
     public static String getCookieFilePath() {
+        // 获取系统类型
         String osName = System.getProperty("os.name").toLowerCase();
+        // 根据系统选择路径
         if (osName.contains("windows")) {
+            // Windows
             String usrdir = System.getenv("USERPROFILE");
             return usrdir + "\\.openbili\\cookie.txt";
         } else if (osName.contains("mac") || osName.contains("linux")) {
+            // 类 Unix
             String usrHome = System.getProperty("user.home");
             return usrHome + "/.openbili/cookie.txt";
         } else {
-            // 其他操作系统
-            Logger.println("未知的操作系统, Cookie 将保存在当前工作目录下",2);
+            // 其她
+            Logger.println("未知的操作系统 "+osName+", Cookie 将保存在当前工作目录下",2);
             return "cookie.txt";
         }
     }
     
     public static void save(String cookie) throws IOException {
-        // 准备文件
-        FileWriter writer = new FileWriter(getCookieFilePath());
-        // 写入文件
-        writer.write(System.currentTimeMillis() + "\n" + cookie);
-        writer.close();
+        // 验证获取的 Cookie
+        if(cookie==null||cookie.trim().isEmpty()) {
+            // Cookie 为空
+            Logger.println("Cookie 为空",2);
+        } else {
+            // 获取路径
+            String path = getCookieFilePath();
+            // 检查父目录
+            File parentDir = new File(path).getParentFile();
+            if(!parentDir.exists()) {
+                parentDir.mkdirs();
+            }
+            // 写入文件
+            try(FileWriter writer = new FileWriter(path)) {
+                writer.write(System.currentTimeMillis() + "\n" + cookie);
+            } catch(Exception e) {
+                Logger.println("Cookie 文件写入失败",2);
+                OutFormat.outException(e,2);
+            }
+        }
     }
 
     public static String load() throws IOException {
