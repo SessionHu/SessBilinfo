94982a8f71fda185b3d03f0096a9b7c4e39a67f8
SessionHu <102411014+SessionHu@users.noreply.github.com>
2023-09-23 16:27:20 +0800

    chore: 进一步调整快速构建流程

--- buildquick.sh
+++ buildquick.sh
@@ -3,11 +3,11 @@ javac -encoding utf-8 \
       -d build/ \
       -cp lib/gson-2.10.1.jar \
       -sourcepath src/java/tk/xhuoffice/sessbilinfo/ \
+      src/java/tk/xhuoffice/sessbilinfo/ui/*.java \
       src/java/tk/xhuoffice/sessbilinfo/util/*.java \
       src/java/tk/xhuoffice/sessbilinfo/*.java
 
 if [ $? -eq 0 ]; then
-    cp ./NOTES.md ./README.md ./LICENSE ./RELEASE.md build/
     cd build/
     jar -cvfm 'SessBilinfo.jar' ../manifest -C ./ .
     exit 0
