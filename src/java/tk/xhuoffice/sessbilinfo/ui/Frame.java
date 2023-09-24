5d0c03cccadd7e8298451f24f46b034e1abe570c
SessionHu <102411014+SessionHu@users.noreply.github.com>
2023-09-24 12:35:56 +0800

    feat(Frame): 给标题添加白色背景黑色前景颜色

--- src/java/tk/xhuoffice/sessbilinfo/ui/Frame.java
+++ src/java/tk/xhuoffice/sessbilinfo/ui/Frame.java
@@ -1,8 +1,5 @@
 package tk.xhuoffice.sessbilinfo.ui;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import tk.xhuoffice.sessbilinfo.Main;
 import tk.xhuoffice.sessbilinfo.util.Logger;
 import tk.xhuoffice.sessbilinfo.util.OutFormat;
@@ -19,33 +16,7 @@ public class Frame {
         Main.cmdArgs(args);
         // 启动!
         clear();
-        printFullTitle();
-        ExecutorService executor = Executors.newFixedThreadPool(2);
-        Future update = executor.submit(() -> update());
-        Future process = executor.submit(() -> process());
-        try {
-            update.get();
-            process.get();
-        } catch(InterruptedException|java.util.concurrent.ExecutionException e) {
-            OutFormat.outThrowable(e,3);
-        }
-        executor.shutdown();
-    }
-    
-    public static void update() {
-        while(true) {
-            try {
-                Thread.sleep(30000);
-            } catch(InterruptedException e) {
-                // do nothing...
-            }
-            size = Size.get();
-            printFullTitle();
-        }
-    }
-    
-    public static void process() {
-        Logger.ln();
+        printTitle();
         Main.main();
     }
     
@@ -58,33 +29,13 @@ public class Frame {
         }
     }
     
-    public static void clearTitleLine() {
-        System.out.printf("\033[0;0f");
-        for(int c = 0; c < size[0]; c++) {
+    public static void printTitle() {
+        System.out.print("\033[0;0f\033[47;30m");
+        for(int i = 0; i < size[0]; i++) {
             System.out.print(" ");
         }
-    }
-    
-    public static void printFullTitle() {
-        clearTitleLine();
-        printTitle();
-        printTime();
-        System.out.println();
-        Pointer.set();
-    }
-    
-    public static void printTitle() {
         System.out.print("\033[0;0f"+Main.SOFT_TITLE);
-    }
-    
-    public static void printTime() {
-        String time = getFormatDateTime();
-        int length = time.length()-3;
-        System.out.printf("\033[0;%df%s",size[0]-length,time.substring(0,length));
-    }
-    
-    public static String getFormatDateTime() {
-        return OutFormat.fullDateTime(System.currentTimeMillis()/1000);
+        System.out.println("\033[0m");
     }
     
 }
