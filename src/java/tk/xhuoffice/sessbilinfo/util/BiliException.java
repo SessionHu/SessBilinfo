239bdd8e7cbc3b6518a14e74b991ac4cf9d9e858
SessionHu <102411014+SessionHu@users.noreply.github.com>
2023-10-06 16:22:41 +0800

    feat(BiliException): cause

--- src/java/tk/xhuoffice/sessbilinfo/util/BiliException.java
+++ src/java/tk/xhuoffice/sessbilinfo/util/BiliException.java
@@ -1,15 +1,24 @@
 package tk.xhuoffice.sessbilinfo.util;
 
+
 public class BiliException extends RuntimeException {
     
     // 异常信息
     private String message;
+    
+    private Throwable cause;
     
     public BiliException() {}
     
     public BiliException(String message) {
         super(message);
         this.message = message;
+    }
+    
+    public BiliException(String message, Throwable cause) {
+        super(message,cause);
+        this.message = message;
+        this.cause = cause;
     }
     
 }
