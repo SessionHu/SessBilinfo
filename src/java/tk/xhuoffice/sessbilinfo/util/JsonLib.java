bd1b03225b9746e835cf264946bc9a8135ef3bb5
SessionHu <102411014+SessionHu@users.noreply.github.com>
2023-11-19 18:01:30 +0800

    perf(JsonLib): 提高代码重用率

--- src/java/tk/xhuoffice/sessbilinfo/util/JsonLib.java
+++ src/java/tk/xhuoffice/sessbilinfo/util/JsonLib.java
@@ -10,43 +10,57 @@ import java.util.stream.StreamSupport;
 
 public class JsonLib {
     
-    private static final Gson GSON = new Gson();
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().setDateFormat("yyyy-MM-dd").create();
     
     // 格式化 JSON
     public static String formatJson(String inputJson) {
-        return new GsonBuilder().setPrettyPrinting().create().toJson(GSON.fromJson(inputJson,JsonObject.class));
+        return GSON.toJson(GSON.fromJson(inputJson,JsonObject.class));
     }
     
     private static JsonElement getJsonElement(String inputJson, String[] path) {
-        JsonElement element = GSON.fromJson(inputJson, JsonObject.class);
-        for(String key : path) {
-            element = element.getAsJsonObject().get(key);
-        }
-        return element;
-    }
-    
-    // 获取 String
-    public static String getString(String inputJson, String... path) {
         try {
-            return getJsonElement(inputJson,path).getAsString();
-        } catch(NullPointerException e) {
+            JsonElement element = GSON.fromJson(inputJson, JsonObject.class);
+            for(String key : path) {
+                if(element==null || !element.getAsJsonObject().has(key)) {
+                    return null;
+                } else {
+                    element = element.getAsJsonObject().get(key);
+                }
+            }
+            return element;
+        } catch(com.google.gson.JsonSyntaxException e) {
+            OutFormat.outThrowable(e,3);
             return null;
         }
     }
     
+    public static <T>T get(String inputJson, Class<T> type, String[] path) {
+        JsonElement element = getJsonElement(inputJson,path);
+        if(element!=null) {
+            return GSON.fromJson(element,type);
+        } else {
+            return null;
+        }
+    }
+    
+    // 获取 String
+    public static String getString(String inputJson, String... path) {
+        return get(inputJson,String.class,path);
+    }
+    
     // 获取 int
     public static int getInt(String inputJson, String... path) {
-        return getJsonElement(inputJson,path).getAsInt();
+        return get(inputJson,int.class,path);
     }
     
     // 获取 long
     public static long getLong(String inputJson, String... path) {
-        return getJsonElement(inputJson,path).getAsLong();
+        return get(inputJson,long.class,path);
     }
     
     // 获取 float
     public static float getFloat(String inputJson, String... path) {
-        return getJsonElement(inputJson,path).getAsFloat();
+        return get(inputJson,float.class,path);
     }
     
     // 获取 Json Array 中的内容作为 String[]
