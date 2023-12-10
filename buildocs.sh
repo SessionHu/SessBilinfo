4958af43f6e8a76487ccbb2e3f63405fe470b31f
SessionHu <102411014+SessionHu@users.noreply.github.com>
2023-12-10 17:32:19 +0800

    chore(buildocs.sh): javadoc

---
+++ buildocs.sh
@@ -0,0 +1,9 @@
+javadoc -cp "./lib/gson-2.10.1.jar:./lib/jansi-2.4.1.jar:./src/java" \
+        -docfilessubdirs -use -author \
+        -d docs \
+        -link "https://docs.oracle.com/javase/8/docs/api/" \
+        -source 8 \
+        -encoding UTF-8 \
+        -charset UTF-8 \
+        -docencoding UTF-8 \
+        tk.xhuoffice.sessbilinfo tk.xhuoffice.sessbilinfo.net tk.xhuoffice.sessbilinfo.theme tk.xhuoffice.sessbilinfo.ui tk.xhuoffice.sessbilinfo.util
