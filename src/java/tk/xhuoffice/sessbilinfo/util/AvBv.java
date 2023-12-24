9169f9d676115eb0d20bff088b229862cb251f79
SessionHu <102411014+SessionHu@users.noreply.github.com>
2023-12-24 16:00:56 +0800

    feat(AvBv): override hashCode()

--- src/java/tk/xhuoffice/sessbilinfo/util/AvBv.java
+++ src/java/tk/xhuoffice/sessbilinfo/util/AvBv.java
@@ -5,8 +5,10 @@ import java.util.Map;
 import java.util.Objects;
 
 /**
- * 原始代码来自: https://github.com/SocialSisterYi/bilibili-API-collect/blob/master/docs/misc/bvid_desc.md#Java
- * 原始算法来自: https://www.zhihu.com/question/381784377/answer/1099438784
+ * Convert Bilibili video Bvid into Aid and back.
+ * @see <a href="https://github.com/SocialSisterYi/bilibili-API-collect/blob/master/docs/misc/bvid_desc.md#Java">bvid说明
+</a>
+ * @see <a href="https://www.zhihu.com/question/381784377/answer/1099438784">如何看待 2020 年 3 月 23 日哔哩哔哩将稿件的「av 号」变更为「BV 号」？</a>
  */
 
 public class AvBv {
@@ -26,20 +28,27 @@ public class AvBv {
         return this.bvid;
     }
     
-    // 构造函数
+    /**
+     * Basic constructor.
+     */
     public AvBv() {
         Logger.debugln("AvBv转换实用工具");
-        for(int i = 0; i < 58; i++) {
-            MAP.put(TABLE.charAt(i), i);
-        }
     }
-
+    
+    /**
+     * Construct with {@code aid}.
+     * @param aid Aid
+     */
     public AvBv(int aid) {
         this();
         this.aid = aid;
         this.bvid = aidToBvid(aid);
     }
-
+    
+    /**
+     * Construct with {@code bvid}.
+     * @param bvid Bvid with {@code .toLowerCase().startsWith("bv")==true}
+     */
     public AvBv(String bvid) {
         this();
         this.bvid = bvid;
@@ -47,11 +56,16 @@ public class AvBv {
     }
     
     // 几个常量
-    private final String TABLE = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF";
-    private final int[] S = new int[]{11, 10, 3, 8, 4, 6};
-    private final int XOR = 177451812;
-    private final long ADD = 8728348608L;
-    private final Map<Character, Integer> MAP = new HashMap<>();
+    private static final String TABLE = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF";
+    private static final int[] S = new int[]{11, 10, 3, 8, 4, 6};
+    private static final int XOR = 177451812;
+    private static final long ADD = 8728348608L;
+    private static final Map<Character, Integer> MAP = new HashMap<>();
+    static {
+        for(int i = 0; i < 58; i++) {
+            MAP.put(TABLE.charAt(i), i);
+        }
+    }
     
     // 转换方法
     public String aidToBvid(int aid) {
@@ -102,6 +116,16 @@ public class AvBv {
             return (avbv.aid==this.aid)&&Objects.equals(avbv.bvid,this.bvid);
         }
         return false;
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1454;
+        result = prime * result + this.aid;
+        result = prime * result + this.bvid.hashCode();
+        result = prime * result + this.toString().hashCode();
+        return result;
     }
     
 }
