b1a34ab0886fcdef34a32875847feeb1ced2ec8b
SessionHu <102411014+SessionHu@users.noreply.github.com>
2023-12-24 19:18:40 +0800

    chore: v1.1.0-beta.2

--- src/java/tk/xhuoffice/sessbilinfo/Main.java
+++ src/java/tk/xhuoffice/sessbilinfo/Main.java
@@ -12,7 +12,7 @@ import tk.xhuoffice.sessbilinfo.util.OutFormat;
 public class Main {
     
     public static final String SOFT_NAME = "SessBilinfo";
-    public static final String SOFT_VERSION = "1.1.0-beta.1";
+    public static final String SOFT_VERSION = "1.1.0-beta.2";
     public static final String SOFT_TITLE  = SOFT_NAME+" "+SOFT_VERSION;
     
     public static void main(String... args) {
