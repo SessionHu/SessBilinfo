f212c5eff378076a6a171b5d1914e1826209db50
SessionHu <102411014+SessionHu@users.noreply.github.com>
2024-01-21 17:50:58 +0800

    chore(buildocs.sh): 用于在 Java17+ 生成文档

--- buildocs.sh
+++ buildocs.sh
@@ -1,8 +1,9 @@
-javadoc -cp "./lib/gson-2.10.1.jar:./lib/jansi-2.4.1.jar:./src/java" \
+javadoc -cp "./lib/gson-2.10.1.jar:./lib/jansi-2.4.1.jar:./lib/jline-3.24.1.jar:./src/java" \
         -docfilessubdirs -author \
         -d docs \
         -link "https://docs.oracle.com/javase/8/docs/api/" \
-        -source 8 \
+        -link "https://www.javadoc.io/static/org.jline/jline/3.25.0/" \
+        -link "https://www.javadoc.io/static/com.google.code.gson/gson/2.10.1/" \
         -header "SessBilinfo<br />(OpenBili.)" \
         -footer "SessBilinfo<br />(OpenBili.)" \
         -bottom "(C) 2023-2024 SessionHu" \
