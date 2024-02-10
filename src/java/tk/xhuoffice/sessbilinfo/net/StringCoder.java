2dd6df7e9e3250ab3aa937caba9d07b3a1b91b57
Session小胡 <102411014+SessionHu@users.noreply.github.com>
2024-02-10 15:07:35 +0800

    feat(StringCoder): MD5

--- src/java/tk/xhuoffice/sessbilinfo/net/StringCoder.java
+++ src/java/tk/xhuoffice/sessbilinfo/net/StringCoder.java
@@ -1,10 +1,13 @@
 package tk.xhuoffice.sessbilinfo.net;
 
+import java.math.BigInteger;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 import java.util.Base64;
+import tk.xhuoffice.sessbilinfo.util.OutFormat;
 
 /**
  * Encode or decode String in Base64 or URL.
@@ -62,6 +65,24 @@ public class StringCoder {
      */
     public static String base64Decode(String str) {
         return new String(BASE64DECODER.decode(str.getBytes(UTF_8)),UTF_8);
+    }
+
+    /**
+     * MD5 Message Digest.
+     * @param str Input message
+     * @return    Digest
+     */
+    public static String md5(String str) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(str.getBytes(UTF_8));
+            String result = new BigInteger(1,md.digest()).toString(16);
+            md.reset();
+            return result;
+        } catch(java.security.NoSuchAlgorithmException e) {
+            OutFormat.outThrowable(e,3);
+            return null;
+        }
     }
     
 }
